6b92b7be239670a0283ccbdcbc69e4dfd74a7e02
ThatBritishOne <wattsj1999@gmail.com>
2026-08-13 15:59:27 +0100

    Fix Issue with Disk Detect

--- internal/migrationdetect/disk_windows.go
+++ internal/migrationdetect/disk_windows.go
@@ -3,5 +3,5 @@ package migrationdetect
 import "errors"
 
 func freeSpace(path string) (int64, error) {
-	return 0, errors.New("filesystem free-space detection is only available on Linux")
+	return 0, errors.New("filesystem free-space detection is not supported on Windows")
 }
